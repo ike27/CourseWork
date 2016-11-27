101c1fde3d4d1c3ad271accb356d4588e8f0f4b0
ike27 <ike.gabrielyan@yandex.ru>
2016-11-27 15:08:20 +0300

    заработала сериализация

--- WordTemplate/WordTemplate/Form1.cs
+++ WordTemplate/WordTemplate/Form1.cs
@@ -8,7 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.Serialization.Formatters;
-    
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
 namespace WordTemplate
 {
     public partial class Form1 : Form
@@ -18,54 +20,51 @@ namespace WordTemplate
             InitializeComponent();
             
         }
+
         TemplateData tdata = new TemplateData();
+
         private void button1_Click(object sender, EventArgs e)
         {
            
             if (textBox1.Text != "") tdata.institute.Add(textBox1.Text);
-            if (textBox2.Text != "") tdata.department.Add(textBox1.Text);
-            if (textBox3.Text != "") tdata.theme.Add(textBox1.Text);
-            if (textBox4.Text != "") tdata.code.Add(textBox1.Text);
-            if (textBox5.Text != "") tdata.specialization.Add(textBox1.Text);
-            if (textBox6.Text != "") tdata.section.Add(textBox1.Text);
-            if (textBox7.Text != "") tdata.student.Add(textBox1.Text);
-            if (textBox8.Text != "") tdata.head_dep_name.Add(textBox1.Text);
-            if (textBox9.Text != "") tdata.head_dep_degree.Add(textBox1.Text);
-            if (textBox10.Text != "") tdata.adviser_name.Add(textBox1.Text);
-            if (textBox11.Text != "") tdata.adviser_degree.Add(textBox1.Text);
-            foreach (string t in tdata.theme)
-            {
-                Console.WriteLine(t);
-            }
+            if (textBox2.Text != "") tdata.department.Add(textBox2.Text);
+            if (textBox3.Text != "") tdata.theme.Add(textBox3.Text);
+            if (textBox4.Text != "") tdata.code.Add(textBox4.Text);
+            if (textBox5.Text != "") tdata.specialization.Add(textBox5.Text);
+            if (textBox6.Text != "") tdata.section.Add(textBox6.Text);
+            if (textBox7.Text != "") tdata.student.Add(textBox7.Text);
+            if (textBox8.Text != "") tdata.head_dep_name.Add(textBox8.Text);
+            if (textBox9.Text != "") tdata.head_dep_degree.Add(textBox9.Text);
+            if (textBox10.Text != "") tdata.adviser_name.Add(textBox10.Text);
+            if (textBox11.Text != "") tdata.adviser_degree.Add(textBox11.Text);      
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
-            //    TemplateData tdata = new TemplateData();
+            foreach (string t in tdata.theme)
+            {
+                MessageBox.Show(t);
+            }
 
-            /*    if 
+            BinaryFormatter formatter = new BinaryFormatter();
 
-                BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("template_data.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, tdata);
+            }
+        }
 
-                using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
-                {
-                    // сериализуем весь массив people
-                    formatter.Serialize(fs, people);
-
-                    Console.WriteLine("Объект сериализован");
-                }
-
-                // десериализация
-                using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
-                {
-                    Person[] deserilizePeople = (Person[])formatter.Deserialize(fs);
-
-                    foreach (Person p in deserilizePeople)
-                    {
-                        Console.WriteLine("Имя: {0} --- Возраст: {1}", p.Name, p.Age);
-                    }
-                }*/
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("template_data.dat", FileMode.OpenOrCreate))
+            {
+                tdata = (TemplateData)formatter.Deserialize(fs);          
+            }
+            foreach (string t in tdata.theme)
+            {
+                MessageBox.Show(t);
+            }
         }
     }
 
