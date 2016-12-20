c945d8d83bb5a2c71b4426a20e8e98079296af2c
ike27 <ike.gabrielyan@yandex.ru>
2016-12-20 23:42:58 +0300

    Lab1

--- WordTemplate/WordTemplate/Form1.cs
+++ WordTemplate/WordTemplate/Form1.cs
@@ -26,32 +26,51 @@ namespace WordTemplate
         }
 
         private TemplateData tdata = new TemplateData();
-        private string _contName = "";
-        private string _sdtPropId = "";
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ComboBox_Update()
         {
-
-            if (textBox1.Text != "") tdata.institute.Add(textBox1.Text);
-            if (textBox2.Text != "") tdata.department.Add(textBox2.Text);
-            if (textBox3.Text != "") tdata.theme.Add(textBox3.Text);
-            if (textBox4.Text != "") tdata.code.Add(textBox4.Text);
-            if (textBox5.Text != "") tdata.specialization.Add(textBox5.Text);
-            if (textBox6.Text != "") tdata.section.Add(textBox6.Text);
-            if (textBox7.Text != "") tdata.student.Add(textBox7.Text);
-            if (textBox8.Text != "") tdata.head_dep_name.Add(textBox8.Text);
-            if (textBox9.Text != "") tdata.head_dep_degree.Add(textBox9.Text);
-            if (textBox10.Text != "") tdata.adviser_name.Add(textBox10.Text);
-            if (textBox11.Text != "") tdata.adviser_degree.Add(textBox11.Text);
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            comboBox4.Items.Clear();
+            comboBox5.Items.Clear();
+            comboBox6.Items.Clear();
+            comboBox7.Items.Clear();
+            comboBox8.Items.Clear();
+            comboBox9.Items.Clear();
+            comboBox10.Items.Clear();
+            comboBox11.Items.Clear();
+            for (int i = 0; i < tdata.institute.Count; i++) comboBox1.Items.Add(tdata.institute[i]);
+            for (int i = 0; i < tdata.department.Count; i++) comboBox2.Items.Add(tdata.department[i]);
+            for (int i = 0; i < tdata.theme.Count; i++) comboBox3.Items.Add(tdata.theme[i]);
+            for (int i = 0; i < tdata.code.Count; i++) comboBox4.Items.Add(tdata.code[i]);
+            for (int i = 0; i < tdata.specialization.Count; i++) comboBox5.Items.Add(tdata.specialization[i]);
+            for (int i = 0; i < tdata.section.Count; i++) comboBox6.Items.Add(tdata.section[i]);
+            for (int i = 0; i < tdata.student.Count; i++) comboBox7.Items.Add(tdata.student[i]);
+            for (int i = 0; i < tdata.head_dep_name.Count; i++) comboBox8.Items.Add(tdata.head_dep_name[i]);
+            for (int i = 0; i < tdata.head_dep_degree.Count; i++) comboBox9.Items.Add(tdata.head_dep_degree[i]);
+            for (int i = 0; i < tdata.adviser_name.Count; i++) comboBox10.Items.Add(tdata.adviser_name[i]);
+            for (int i = 0; i < tdata.adviser_degree.Count; i++) comboBox11.Items.Add(tdata.adviser_degree[i]);
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            foreach (string t in tdata.theme)
-            {
-                MessageBox.Show(t);
-            }
+            if ((textBox1.Text != "") && !tdata.institute.Contains(textBox1.Text)) tdata.institute.Add(textBox1.Text);
+            if ((textBox2.Text != "") && !tdata.department.Contains(textBox2.Text)) tdata.department.Add(textBox2.Text);
+            if ((textBox3.Text != "") && !tdata.theme.Contains(textBox3.Text)) tdata.theme.Add(textBox3.Text);
+            if ((textBox4.Text != "") && !tdata.code.Contains(textBox4.Text)) tdata.code.Add(textBox4.Text);
+            if ((textBox5.Text != "") && !tdata.specialization.Contains(textBox5.Text)) tdata.specialization.Add(textBox5.Text);
+            if ((textBox6.Text != "") && !tdata.section.Contains(textBox6.Text)) tdata.section.Add(textBox6.Text);
+            if ((textBox7.Text != "") && !tdata.student.Contains(textBox7.Text)) tdata.student.Add(textBox7.Text);
+            if ((textBox8.Text != "") && !tdata.head_dep_name.Contains(textBox8.Text)) tdata.head_dep_name.Add(textBox8.Text);
+            if ((textBox9.Text != "") && !tdata.head_dep_degree.Contains(textBox9.Text)) tdata.head_dep_degree.Add(textBox9.Text);
+            if ((textBox10.Text != "") && !tdata.adviser_name.Contains(textBox10.Text)) tdata.adviser_name.Add(textBox10.Text);
+            if ((textBox11.Text != "") && !tdata.adviser_degree.Contains(textBox11.Text)) tdata.adviser_degree.Add(textBox11.Text);
+            ComboBox_Update();
+        }
 
+        private void button2_Click_1(object sender, EventArgs e)
+        {
             BinaryFormatter formatter = new BinaryFormatter();
 
             using (FileStream fs = new FileStream("template_data.dat", FileMode.OpenOrCreate))
@@ -60,95 +79,111 @@ namespace WordTemplate
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click_1(object sender, EventArgs e)
         {
             BinaryFormatter formatter = new BinaryFormatter();
             using (FileStream fs = new FileStream("template_data.dat", FileMode.OpenOrCreate))
             {
                 tdata = (TemplateData)formatter.Deserialize(fs);
             }
-            foreach (string t in tdata.theme)
-            {
-                MessageBox.Show(t);
-            }
+            ComboBox_Update();           
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open("C:\\Users\\ikega\\Desktop\\ООП\\kurs1.docx", true)){ 
-            foreach (var item in wordDoc.MainDocumentPart.Document.Body)
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 0;
+            comboBox5.SelectedIndex = 0;
+            comboBox6.SelectedIndex = 0;
+            comboBox7.SelectedIndex = 0;
+            comboBox8.SelectedIndex = 0;
+            comboBox9.SelectedIndex = 0;
+            comboBox10.SelectedIndex = 0;
+            comboBox11.SelectedIndex = 0;
+            DateTime now = DateTime.Now;
+            textBox13.Text = now.ToString("yyyy");
+        }
+
+        private void buttonTRY_Click(object sender, EventArgs e)
+        {
+            richTextBox1.LoadFile("template_vkr_bachelor.rtf");
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open("template_vkr_bachelor.docx", true))
             {
-                var oo = item.Descendants<SdtProperties>();
-                foreach (var f1 in oo)
+                foreach (var item in wordDoc.MainDocumentPart.Document.Body)
                 {
-                    Tag tag = f1.Elements<Tag>().FirstOrDefault();
-                    string _tag = "";
-
-                    if (tag != null) _tag = tag.Val;
-
-                    if (_tag.Contains("_institute")) // #1
+                    var oo = item.Descendants<SdtProperties>();
+                    foreach (var f1 in oo)
                     {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox1.Text;
-                    }
-                    if (_tag.Contains("_department")) // #2
+                        Tag tag = f1.Elements<Tag>().FirstOrDefault();
+                        string _tag = "";
+
+                        if (tag != null) _tag = tag.Val;
+
+                        if (_tag.Contains("_institute")) // #1
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox2.Text;
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox1.SelectedItem.ToString();
                         }
-                    if (_tag.Contains("_theme")) // #3
+                        if (_tag.Contains("_department")) // #2
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox3.Text;
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox2.SelectedItem.ToString(); 
                         }
-                    if (_tag.Contains("_code")) // #4
+                        if (_tag.Contains("_theme")) // #3
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox4.Text;
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox3.SelectedItem.ToString();
                         }
-                    if (_tag.Contains("_specialization")) // #5
+                        if (_tag.Contains("_code")) // #4
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox5.Text;
-                    }
-                    if (_tag.Contains("_section")) // #6
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox4.SelectedItem.ToString();
+                        }
+                        if (_tag.Contains("_specialization")) // #5
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox6.Text;
-                    }
-                    if (_tag.Contains("_student")) // #7
-                    {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox7.Text;
-                    }
-                    if (_tag.Contains("_head_dep_name")) // #8
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox5.SelectedItem.ToString();
+                        }
+                        if (_tag.Contains("_section")) // #6
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox8.Text;
-                    }
-                    if (_tag.Contains("_head_dep_degree")) // #9
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox6.SelectedItem.ToString();
+                        }
+                        if (_tag.Contains("_student")) // #7
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox9.Text;
-                    }
-                    if (_tag.Contains("_adviser_name")) // #10
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox7.SelectedItem.ToString();
+                        }
+                        if (_tag.Contains("_head_dep_name")) // #8
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox10.Text;
-                    }
-                    if (_tag.Contains("_adviser_degree")) // #11
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox8.SelectedItem.ToString();
+                        }
+                        if (_tag.Contains("_head_dep_degree")) // #9
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox11.Text;
-                    }
-                    if (_tag.Contains("_year")) // #12
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox9.SelectedItem.ToString();
+                        }
+                        if (_tag.Contains("_adviser_name")) // #10
                         {
-                        SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>()?.Val == _tag);
-                        element.Descendants<Text>().First().Text = textBox12.Text;
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox10.SelectedItem.ToString();
+                        }
+                        if (_tag.Contains("_adviser_degree")) // #11
+                        {
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = comboBox11.SelectedItem.ToString();
+                        }
+                        if (_tag.Contains("_year")) // #12
+                        {
+                            SdtElement element = wordDoc.MainDocumentPart.Document.Body.Descendants<SdtElement>().FirstOrDefault(sdt => sdt.SdtProperties.GetFirstChild<Tag>() != null ? sdt.SdtProperties.GetFirstChild<Tag>().Val == _tag : false);
+                            element.Descendants<Text>().First().Text = textBox13.Text;
+                        }
                     }
                 }
-            }
-            wordDoc.Close();
+                wordDoc.Close();
             }
         }
 
